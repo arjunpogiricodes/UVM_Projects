52f0aaddd94492d285afeba5c7dd00f79c1894db
ARJUN POGIRI <122292108+arjunpogiricodes@users.noreply.github.com>
2024-12-02 22:57:02 +0530

    Add files via upload

---
+++ Lab03/packages/ram_pkg.sv
@@ -0,0 +1,20 @@
+
+
+package ram_pkg;
+
+	// import the UVM package
+       import uvm_pkg :: *;
+
+	// include the uvm_macros.svh
+	      `include "uvm_macros.svh"
+	// include the tb_defs.sv
+             `include "tb_defs.sv"
+ 
+	// include ram_wr_driver,ram_wr_agent,ram_env,ram_wr_test files
+            `include "ram_wr_driver.sv"   
+	    `include "ram_wr_agent.sv"
+            `include "ram_env.sv"
+	    `include "ram_wr_test.sv"		
+	
+
+endpackage
